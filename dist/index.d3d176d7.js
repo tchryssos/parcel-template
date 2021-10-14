081223ee121255f35546cc9556b597b885163b5a
Troy Chryssos <troychryssos@gmail.com>
2021-10-13 23:10:18 -0400

    updating build and prod command to properly create builds

---
+++ dist/index.d3d176d7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("content");t&&(t.textContent="TEMPLATE WORKS!")}();
+//# sourceMappingURL=index.d3d176d7.js.map
---
+++ dist/index.e422049e.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("content");t&&(t.textContent="TEMPLATE WORKS!");
+//# sourceMappingURL=index.e422049e.js.map
